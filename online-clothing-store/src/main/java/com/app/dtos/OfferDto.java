da2e1f9bb09c186b078e5aede60183ec4f3714c8
Ankush-R-Jain <arsanklecha2001@gmail.com>
2023-08-18 15:44:29 +0530

    dependencies added

--- online-clothing-store/src/main/java/com/app/dtos/OfferDto.java
+++ online-clothing-store/src/main/java/com/app/dtos/OfferDto.java
@@ -2,10 +2,11 @@ package com.app.dtos;
 
 import java.sql.Date;
 
-import com.app.pojos.Category;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
+import com.app.pojos.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,13 +20,28 @@ import lombok.ToString;
 @ToString
 public class OfferDto {
 
-	private String offerName;
-	private String offerDescription;
-	private double discountAmount;
-	private String promoCode;
-	private Date startDate;
-	private Date endDate;
-	private Category category;
-	private double minimumPurchaseAmount;
+    @NotBlank(message = "Offer name is required")
+    private String offerName;
+
+    @NotBlank(message = "Offer description is required")
+    private String offerDescription;
+
+    @Positive(message = "Discount amount must be positive")
+    private double discountAmount;
+
+    @NotBlank(message = "Promo code is required")
+    private String promoCode;
+
+    @NotNull(message = "Start date is required")
+    private Date startDate;
+
+    @NotNull(message = "End date is required")
+    private Date endDate;
+
+    @NotNull(message = "Category is required")
+    private Category category;
+
+    @Positive(message = "Minimum purchase amount must be positive")
+    private double minimumPurchaseAmount;
 
 }
